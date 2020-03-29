db4417be4bd056ce94e4f8eb32ee917b7721de1a
huiping_guo <huiping.guo@dena.jp>
2020-03-29 16:16:07 +0900

    apply gitignore which remove Pods dic

--- HPLibRTMP/Example/Pods/Target Support Files/HPLibRTMP/HPLibRTMP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HPLibRTMP : NSObject
-@end
-@implementation PodsDummy_HPLibRTMP
-@end
--- HPLibRTMP/Example/Pods/Target Support Files/Pods-HPLibRTMP_Tests/Pods-HPLibRTMP_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HPLibRTMP_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HPLibRTMP_Tests
-@end
